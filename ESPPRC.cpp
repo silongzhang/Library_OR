dccb26945bde9c155c27d6c3b2317c8519f4641d
along <along1994@sjtu.edu.cn>
2020-10-19 11:54:39 +0800

    2020.10.19.11.54

--- ESPPRC.cpp
+++ ESPPRC.cpp
@@ -1,6 +1,6 @@
 #include"ESPPRC.h"
 
-// check whether this label can extend to vertex j
+// Check whether this label can extend to vertex j.
 bool Label_ESPPRC::canExtend(const int j) const {
 	return !unreachable.test(j);
 }
--- ESPPRC.h
+++ ESPPRC.h
@@ -1,13 +1,27 @@
 #pragma once
 
+/*
+
+It is a procedure for solving ESPPRC, where quantity, distance and time resources are considered.
+
+Important Assumptions:
+1. Both the origin and destination locate in the depot, and the depot is vertex 0.
+2. Any feasible solution should contain at least one vertex in addition to the origin and destination.
+3. Resource consumption always is nonnegative and satisfies triangle inequalities.
+
+*/
+
 #include"Header.h"
 #include"define.h"
 
 constexpr auto Max_Num_Vertex = 128;
 
+class Label_ESPPRC;
+
 class Data_ESPPRC {
 public:
-	vector<vector<bool>> ExistingArcs;
+	// The number of vertices, including the depot.
+	int NumVertices;
 	vector<vector<QuantityType>> Quantity;
 	vector<pair<QuantityType, QuantityType>> QuantityWindow;
 	vector<vector<DistanceType>> Distance;
@@ -16,6 +30,21 @@ public:
 	vector<pair<TimeType, TimeType>> TimeWindow;
 	vector<vector<double>> RealCost;
 	vector<vector<double>> ReducedCost;
+	// UnreachableForever[i].test(j) = true if and only if sequences {...,i,...,j,...} 
+	// are always infeasible due to resource constraints.
+	vector<bitset<Max_Num_Vertex>> UnreachableForever;
+	// ExistingArcs[i][j] = false if and only if: (1) i = j or (2) UnreachableForever[i].test(j) = true 
+	// or (3) arc (i,j) dose not exist in the network.
+	vector<vector<bool>> ExistingArcs;
+	// Lower bounds.
+	vector<vector<double>> LBQI;
+	vector<vector<double>> LBDI;
+	vector<vector<double>> LBTI;
+	vector<vector<vector<vector<double>>>> LBQDTI;
+	// Data structures for saving labels.
+	vector<unordered_map<bitset<Max_Num_Vertex>, list<Label_ESPPRC>>> pastIU;
+	vector<unordered_map<bitset<Max_Num_Vertex>, list<Label_ESPPRC>>> currentIU;
+	vector<unordered_map<bitset<Max_Num_Vertex>, list<Label_ESPPRC>>> nextIU;
 };
 
 class Consumption_ESPPRC {
@@ -23,22 +52,36 @@ public:
 	QuantityType quantity;
 	DistanceType distance;
 	TimeType time;
+
+	// Renew this object after extending to vertex j.
 };
 
 class Cost_ESPPRC {
 public:
 	double realCost;
 	double reducedCost;
+
+	// Renew this object after extending to vertex j.
 };
 
 class Label_ESPPRC {
-public:
+private:
 	vector<int> path;
-	int tail;									// the last vertex in the path
+	int tail;									// The last vertex in the path.
 	bitset<Max_Num_Vertex> unreachable;
 	Consumption_ESPPRC consumption;
 	Cost_ESPPRC cost;
 
+public:
+	// Constructor.
+	// Check whether this label can extend to vertex j.
 	bool canExtend(const int j) const;
+	// Renew unreachable indicator for vertex j.
+	// Renew unreachable indicators for all vertices.
+	// Extend this lable to vertex j.
+	// Check whether this label is a feasible solution.
 };
+
+// Dominance rule.
+// Bitwise operation for checking whether unreachable lhs is a subset of unreachable rhs.
 
