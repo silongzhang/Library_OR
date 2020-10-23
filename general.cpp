4bb992e25eac2c86a16075970ef04e53c6e4c3e4
along <along1994@sjtu.edu.cn>
2020-10-23 12:18:17 +0800

    2020.10.23.12.18

--- general.cpp
+++ general.cpp
@@ -1,3 +1,15 @@
 #include"general.h"
 
+// Euclidean distance.
+double EuclideanDistance(const double x1, const double y1, const double x2, const double y2) {
+	return sqrt(pow(x1 - x2, 2) + pow(y1 - y2, 2));
+}
+
+
+// Set precision.
+// NB. Ceil function is used.
+double setPrecision(const double data, const int numFloat) {
+	return ceil(data * pow(10, numFloat)) / pow(10, numFloat);
+}
+
 
--- general.h
+++ general.h
@@ -1,7 +1,31 @@
 #pragma once
 
-// A set of general functions
+// A set of general operators
 
 #include"Header.h"
+
+class Customer_Solomon {
+public:
+	int id;
+	double xCoord;
+	double yCoord;
+	double demand;
+	double readyTime;
+	double dueTime;
+	double serviceTime;
+};
+
+class Instance_Solomon {
+public:
+	string name;
+	int numVehicle;
+	double capacity;
+	vector<Customer_Solomon> vertices;
+};
+
+// Euclidean distance.
+double EuclideanDistance(const double x1, const double y1, const double x2, const double y2);
+// Set precision.
+double setPrecision(const double data, const int numFloat);
 
 
